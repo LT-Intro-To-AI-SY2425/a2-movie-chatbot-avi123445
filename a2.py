bdde3e4d1d70d5497d7e8c274315e619a2c34159
avi123445 <avikaplan1000@gmail.com>
2024-10-03 22:20:21 -0500

    Update a2.py

--- a2.py
+++ a2.py
@@ -17,7 +17,6 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     pind = 0  # current index we are looking at in pattern list
     result: List[str] = []  # to store substitutions we will return if matched
 
-    print ("avi test print 1")
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
@@ -29,16 +28,16 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # return ["Not done yet :)"]
 
         # 1) if we reached the end of the pattern but not source
-        print ("Avi test print 2")
+        print ("sind=",sind,"pind=",pind, "result=",result)
         if pind == len(pattern) and sind != len(source):
-            print ("Avi test print 3")
+            print ("None")
             return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
         elif pattern[pind] == '%':
-            print ("Avi test print 4", pind, sind)
+            print ("current thing in the pattern is %")
             if pind == len(pattern) - 1:
                 res = " ".join(source[sind:])
                 result.append(res)
@@ -47,6 +46,7 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
                 # For this part, you'll need to concatenate an accumulate variable that adds a space in between until it find the matching pattern in source.
                 pind += 1
                 accum = ""
+                print ("concatenating an accumulate variable")
                 while pattern[pind] != source[sind]:
                     accum += source[sind] + " "
                     sind += 1
@@ -57,18 +57,18 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
                 # sind = len(source)
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source):
-            print ("Avi test print 5")
+            print ("reached the end of the source but not pattern")
             return None
         # 4) if the current thing in the pattern is an _
         elif pattern[pind] == '_':
-            print ("Avi test print 6")
+            print ("current thing in pattern is _")
             result.append(source[sind])
             sind += 1
             pind += 1
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
         elif source[sind] == pattern[pind]:
-            print ("Avi test print 7")
+            print ("source and pattern matched")
             pind += 1 
             sind += 1
 
@@ -76,11 +76,11 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # indicates the current thing it pattern doesn't match the current thing in
         # source
         else:
-            print ("Avi test print 8")
+            print ("none")
             return None
     return result
 
-print("step 1")
+print("go through all the asserts now...")
 if __name__ == "__main__":
 #    assert match(["what", "movies", "were", "made", "in", "_"],["what", "movies", "were", "made", "in", "1974"]) == ["1974"], "test 1 failed"
 #    assert match(["what", "actors", "were","in","_"], ["what", "actors", "were","in","it"]) == ["lillis"], "test 1 failed"
